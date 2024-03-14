2eee2bab859ea4df9ee65d564414c856dca86645
David <davidortegaboro6667@gmail.com>
2024-03-14 21:02:52 +0100

    Tercer commit Excepciones

--- src/main/java/org/excepciones/ejercicio2/Main.java
+++ src/main/java/org/excepciones/ejercicio2/Main.java
@@ -1,35 +1,32 @@
 package org.excepciones.ejercicio2;
 
 import java.util.Scanner;
-
-class MenorDeEdadException extends Exception{
-    public MenorDeEdadException(){
-        super("La persona es menor de 18 años");
-    }
-}
-
 public class Main {
 
+    //David pregunta las 2C Y 2D
     static Scanner scanner = new Scanner(System.in);
     public static boolean calcularEdad(){
         System.out.print("Introduce tu edad: ");
         int edad = scanner.nextInt();
             if (edad >= 18){
                 System.out.println("Puedes pasar");
-                return true;
-            } else{
-                throw new MenorDeEdadException();
-            }
+            } else if (edad < 18){
+                throw new ArithmeticException("La edad no puede ser menor a 18");
+            } else if (//Tienes que comprobar que el numero no sea entero){
+                throw new RuntimeException("No puede ser negativo");
+        }
+            return true;
     }
 
     public static void main(String[] args){
 
         try{
             System.out.println(calcularEdad());
-        } catch (MenorDeEdadException e){
-            System.out.println("Algo ha salido mal");
+        } catch (ArithmeticException e){
+            System.out.println("Error: " + e.getMessage());
+        } catch (RuntimeException e){
+            System.out.println("Error: " + e.getMessage());
         }
     }
 
-    //David tienes error aquí, vas por el 2b
 }

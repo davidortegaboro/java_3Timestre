53cf3b3a763844a4fa359299c738e629884cbc9c
DavidOrtegaBoro <davidortegaboro6667@gmail.com>
2024-03-15 09:26:07 +0100

    Cuarto

--- src/main/java/org/excepciones/ejercicio2/Main.java
+++ src/main/java/org/excepciones/ejercicio2/Main.java
@@ -1,29 +1,27 @@
 package org.excepciones.ejercicio2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Main {
 
     //David pregunta las 2C Y 2D
     static Scanner scanner = new Scanner(System.in);
-    public static boolean calcularEdad(){
-        System.out.print("Introduce tu edad: ");
-        int edad = scanner.nextInt();
-            if (edad >= 18){
-                System.out.println("Puedes pasar");
-            } else if (edad < 18){
-                throw new ArithmeticException("La edad no puede ser menor a 18");
-            } else if (//Tienes que comprobar que el numero no sea entero){
-                throw new RuntimeException("No puede ser negativo");
+    public static boolean calcularEdad(int edad){
+        if(edad < 18) {
+            throw new RuntimeException("La edad no puede ser menor a 18");
         }
-            return true;
+        return true;
     }
 
     public static void main(String[] args){
-
+        System.out.print("Introduce tu edad: ");
         try{
-            System.out.println(calcularEdad());
-        } catch (ArithmeticException e){
-            System.out.println("Error: " + e.getMessage());
+            Integer edad = scanner.nextInt();
+            if(calcularEdad(edad)) {
+                System.out.println("puedes pasar");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Error: La edad debe ser un entero");
         } catch (RuntimeException e){
             System.out.println("Error: " + e.getMessage());
         }
